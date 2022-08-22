a000feae6249185d9eb9607a85ccadffc4203257
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-08-22 17:11:27 +0000

    added some code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // const bb2 = require('./practice/greatest.js');
-const bb2 = require('./practice/vowel.js');
+//const bb2 = require('./practice/vowel.js');
+const bb2 = require('./practice/test2.js');
 //const bb2 = require('./practice/sort.js');
---
+++ practice/test2.js
@@ -0,0 +1,11 @@
+const x = 5;
+const y = math.random();
+function provRandom(){
+  return x *y;
+}
+
+if(provRandom()== 1){
+  console.log("you are lucky");
+}else{
+  console.log("bad karma!!");
+}
