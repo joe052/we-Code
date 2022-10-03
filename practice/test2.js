35df54c3e3c92e06a4d6ae40dd355e2dea7a00eb
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-10-03 10:45:44 +0000

    replaced a function

--- practice/test2.js
+++ practice/test2.js
@@ -1,14 +1,25 @@
-const maxLimit = 100;
-const y = provRandom(maxLimit);
+function plusMinus(arr) {
+  let x = arr.length;
+  console.log(x);
+  
+  //sort the numbers from positive,negative and zeros
+  let pos = [];
+  let neg = [];
+  let zero = [];
+  
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] < 0){
+      pos.push(arr[i]);
+    }
+    else if(arr[i] === 0){
+      zero.push(arr[i]);
+    }else{
+      neg.push(arr[i]);
+    }
+  }
 
-function provRandom(max){
-  return Math.floor(Math.random()*max);
+  let positive =   Number(pos.length / arr.length).toFixed(6);
+  console.log(positive);
 }
 
-if( y < 50){
-  console.log("you are lucky");
-}else{
-  console.log("bad karma!!");
-}
-
-console.log(y);
+plusMinus([3,5,6,7,8,9]);
