11991d792d14197d84649c4628e1cecc0b5bb2da
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-10-06 14:56:12 +0000

    Removed comment

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const bb2 = require('./practice/greatest.js');
-//const bb2 = require('./practice/vowel.js');
+// const bb2 = require('./practice/vowel.js');
 const bb2 = require('./practice/test2.js');
 //const bb2 = require('./practice/sort.js');
