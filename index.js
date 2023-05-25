672a085c8be5c52bf3d682a115917e725ab18201
joe052 <87515353+joe052@users.noreply.github.com>
2023-05-25 13:31:08 +0000

    commited

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 //requiring file with code to run
 // const bb2 = require('./practice/test2.js');
-const bb2 = require('./practice/calculator.js');
+// const bb2 = require('./practice/calculator.js');
+const bb2 = require('./warmup/server.js');
--- warmup/server.js
+++ warmup/server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.static("warmup"));
 const port = 9000;
+app.listen(port,()=>{
+  console.log("we shall revisit");
+})
